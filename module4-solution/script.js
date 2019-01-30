b9583b896b72233725a3225d00d850fb3af1e681
Ahmet Cangir <ahmetcangir07@gmail.com>
2019-01-31 00:20:35 +0100

    Upload module4-solution

---
+++ module4-solution/script.js
@@ -0,0 +1,28 @@
+// Module 4 Assignment Solution.
+//
+/*
+Expected output should be:
+--------------------------
+Hello Yaakov
+Good Bye John
+Good Bye Jen
+Good Bye Jason
+Hello Paul
+Hello Frank
+Hello Larry
+Hello Paula
+Hello Laura
+Good Bye Jim
+--------------------------
+*/
+(function() {
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+  for (var i = 0; i < names.length; i++) {
+      var firstLetter = names[i].charAt(0).toLowerCase();
+      if (firstLetter === 'j') {
+          byeSpeaker.speak(names[i]);
+      } else {
+          helloSpeaker.speak(names[i]);
+      }
+  }
+})();
